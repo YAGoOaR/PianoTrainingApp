3b83b95b345ba15cb0c427fbc2686c454d7af3c2
Yegor Grybenko <55108635+YAGoOaR@users.noreply.github.com>
2024-03-01 02:39:58 +0200

    Add menu start hotkey

--- Scripts/Menu.cs
+++ Scripts/Menu.cs
@@ -57,6 +57,11 @@ internal partial class Menu : Node2D
 
     public override void _Input(InputEvent @event)
     {
+        if (@event.IsActionPressed("ui_accept"))
+        {
+            OnPlayPressed();
+        }
+
         if (@event.IsActionPressed("ui_cancel"))
         {
             OnQuitPressed();
