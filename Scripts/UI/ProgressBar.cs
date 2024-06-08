2cbcd54d8c54776268d9e9242958a08ae7c81770
Yegor Grybenko <55108635+YAGoOaR@users.noreply.github.com>
2024-06-09 01:58:59 +0300

    Fix progress bar

--- Scripts/UI/ProgressBar.cs
+++ Scripts/UI/ProgressBar.cs
@@ -11,23 +11,15 @@ public partial class ProgressBar : Control
     [Export] private Label progressLabel;
     [Export] private Panel progressPanel;
 
-    private void SetProgressRectBounds(Panel rect, float tStart, float tEnd, float totalTime)
-    {
-        rect.Position = new(Size.X * tStart / totalTime, 0);
-        rect.Size = new(Size.X * (tEnd - tStart) / totalTime, Size.Y);
-    }
-
-    public void SetProgress(float time)
-    {
-        SetProgressRectBounds(progressPanel, 0, time, musicPlayer.TotalSeconds);
-    }
-
     public override void _Process(double delta)
     {
         if (musicPlayer.PlayingState == PlayState.Stopped) return;
 
         var time = musicPlayer.TimeMilis * TimeUtils.MS_TO_SEC;
-        SetProgress(time);
-        progressLabel.Text = $"{time / musicPlayer.TotalSeconds:0%}";
+
+        float progress = Mathf.Clamp(time / musicPlayer.TotalSeconds, 0, 1);
+
+        progressPanel.Size = new(Size.X * progress, Size.Y);
+        progressLabel.Text = $"{progress:0%}";
     }
 }
